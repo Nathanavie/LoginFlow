d0e9fabfa383376947b05e11f2caf07aa40d7c7e
Nathan Atherton <nathanatherton2@gmail.com>
2021-01-14 21:33:37 +0000

    Split out basic states in render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import './App.css';
 
+import Background from './components/Background'
+import Heading from './components/Heading'
+import Paragraph from './components/Paragraph'
+import Button from './components/Button'
+import Form from './components/Form'
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: "false"
+    }
+  }
+
   render() {
-    return (
-      <div id="container">
-        <div class="form">
-          
+    let {loggedIn} = this.state
+    if(loggedIn == "false") {
+      return (
+        <div id="container">
+          <div class="form">
+            <Heading wording="LOGIN" />
+            <Paragraph wording="Login with your Gamma cloud telephone account" span="Gamma"/>
+            <Form />
+            <Button buttonType="toggle" />
+          </div>
+          <div class="background">
+            <Background />
+          </div>
         </div>
-        <div class="background">
-          
+      );
+    } else if(loggedIn) {
+      return (
+        <div id="container">
+          <div class="form">
+            <Heading wording="You are now logged in" />
+            <Paragraph wording="Great stuff"/>
+          </div>
+          <div class="background">
+            <Background />
+          </div>
         </div>
-      </div>
-    );
+      )
+    }
   }
 }
 
