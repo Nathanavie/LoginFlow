9856df47b5395e342e0828d3b32cb16eff411004
Nathan Atherton <nathanatherton2@gmail.com>
2021-01-14 20:45:58 +0000

    Added basic background image component

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,10 @@
-const Backround = props => {
-    return (
-        <>
+import React from 'react';
+import backgroundImage from '../assets/login_illustration.png'
 
-        </>
-    )
+const Background = props => {
+    return (
+        <img src={backgroundImage} alt="Background Image" />
+      )
 }
+export default Background
+  
