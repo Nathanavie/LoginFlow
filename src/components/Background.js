92a56b10c4c95eba3315cdc38f5b5cdfe99b15f0
Nathan Atherton <nathanatherton2@gmail.com>
2021-01-15 00:34:56 +0000

    Change class to className

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Background = props => {
     return (
-        <div class={props.class}></div>
+        <div className={props.class}></div>
       )
 }
 export default Background
